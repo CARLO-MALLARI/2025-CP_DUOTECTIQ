2f8d8bc8dff764eaf75bcb4784b09fa46389cac5
CARLO-MALLARI <carlomallari862@gmail.com>
2025-10-16 06:13:00 +0800

    splash update

--- MainClient/App.tsx
+++ MainClient/App.tsx
@@ -278,12 +278,18 @@
 import { enableScreens } from 'react-native-screens';
 enableScreens(true);
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import RNBootSplash from "react-native-bootsplash";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/providers/AuthProvider';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
+  
+  useEffect(() => {
+    RNBootSplash.hide({ fade: true }); 
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AuthProvider>
--- MainClient/src/components/LoadingScreen.tsx
+++ MainClient/src/components/LoadingScreen.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { View, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, ActivityIndicator, StyleSheet, Image } from 'react-native';
 
 const LoadingScreen = () => (
   <View style={styles.container}>
-    <ActivityIndicator size="large" />
+    <Image
+      source={require('../assets/splash.png')}
+      style={styles.logo}
+      resizeMode="contain"
+    />
   </View>
 );
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#CACACA', // Splash background
+  },
+  logo: {
+    width: 200,   // adjust size as needed
+    height: 200,
+    marginBottom: 20,
+  },
+  loader: {
+    marginTop: 10,
+  },
 });
 
 export default LoadingScreen;
